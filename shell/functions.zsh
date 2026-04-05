defcad4a8ff793ca73858e9e7a062b91bc48dc50
carbe814 <carbe814@student.liu.se>
2026-04-05 15:32:18 +0200

    changed some shell functions

--- shell/functions.zsh
+++ shell/functions.zsh
@@ -1,9 +1,9 @@
 OS=$(uname)
 
-# laddar template-funktionen från template mappen
+# laddar template-funktionen från template-mappen
 source ~/dotfiles/templates/template.zsh
 
-# kommando som abstraherar och förenklar skickandet av ntfy meddelanden
+# kommando som förenklar skickandet av ntfy meddelanden
 function ntfy() {
   if [[ $# -ne 2 ]]; then
     echo "Usage: ntfy <topic> <message>"
@@ -23,31 +23,11 @@ svt() {
         command svt-text "$1"
     else
         # https://github.com/lsjoeberg/textty/releases/tag/v0.1.0
-        command ~/dotfiles/executables/svt-text
+        command ~/dotfiles/bins/svt-txt
     fi
-}
-
-# Function to check video codec of a file
-vcodec() {
-    if [ -z "$1" ]; then
-        echo "Usage: vcodec <file>"
-        return 1
-    fi
-
-    if [ ! -f "$1" ]; then
-        echo "File not found: $1"
-        return 1
-    fi
-
-    ffprobe -v error -select_streams v:0 -show_entries stream=codec_name -of default=nw=1:nk=1 "$1"
 }
 
 # MacOS-specifikt
 # =================================================================================
 if [[ $OS == Darwin ]]; then
-# kommando som flyttar terminalföntret till workspace 3 samtidigt som det öppnar neovim
-# todo: gör att det öppnar ett nytt fönster istället för att flytta det existerande
-    nnv() {
-        command aerospace move-node-to-workspace 3; aerospace workspace 3; nvim "$@"
-    }
 fi
