ec18337703666e66dc68c535786fe555b52b7737
carbe814 <carbe814@student.liu.se>
2026-05-15 22:17:28 +0200

    updates moved from aliases to functions

--- sh/aliases.zsh
+++ sh/aliases.zsh
@@ -10,20 +10,6 @@ alias nv='nvim'
 alias lg='lazygit'
 alias tm='tmux new-session -A -s 0'
 
-mark() {
-  local text=${1:+" $1 "}  # " text " if arg given, empty string if not
-  local width=$COLUMNS
-  local prefix=" * "
-  local suffix=" * "
-  local inner=$(( width - ${#prefix} - ${#suffix} ))
-  local pad=$(( (inner - ${#text}) / 2 ))
-  local extra=$(( inner - ${#text} - pad * 2 ))
-  local bar=$(printf '%*s' $inner '' | tr ' ' '=')
-  local left=${bar:0:$pad}
-  local right=${bar:0:$(( pad + extra ))}
-  printf "\n%s%s%s%s%s\n\n" "$prefix" "$left" "$text" "$right" "$suffix"
-}
-
 # c++ kompilerings-saker
 alias w++17='g++ -std=c++17 -Wall -Wextra -pedantic -g'
 alias e++17='g++ -std=c++17 -Wall -Wextra -pedantic -Werror -g'
@@ -32,6 +18,12 @@ alias e++17='g++ -std=c++17 -Wall -Wextra -pedantic -Werror -g'
 alias wake_server='wakeonlan 6c:4b:90:9f:59:f0'
 alias wake_gaming='wakeonlan FC:34:97:9F:CA:33'
 
+# ===================================== Linux =====================================
+if [[ $OS == Linux ]]; then
+
+
+
+fi
 # ===================================== macOS =====================================
 if [[ $OS == Darwin ]]; then
 
@@ -40,26 +32,9 @@ alias backup-dev='rsync -avzh --delete ~/Dev/ server:/home/carlbergvall/backups/
 alias backup-misc='rsync -avzh --delete ~/Misc/backup/ server:/home/carlbergvall/backups/misc/'
 alias backup-all="mark 'backup-dev' && backup-dev && mark 'backup-misc' && backup-misc"
 
-# Updates commands
-alias buu="brew update && brew upgrade && brew cleanup" # Brew
-alias muu="mas upgrade" # Mas (Mac App Store CLI)
-alias suu="sudo softwareupdate -iaR" # MacOS system update
-alias uu="mark 'Homebrew' && buu && mark 'App Store' && muu && mark 'Software Update' && softwareupdate -l && mark"
-
-fi
-# ===================================== Linux =====================================
-if [[ $OS == Linux ]]; then
-
-
 fi
 # ==================================== Debian =====================================
 if [[ $DISTRO == debian ]]; then
-
-# Update commands
-alias auu="sudo apt update && sudo apt upgrade && sudo apt autoremove" # Apt
-alias nuu="nix registry pin nixpkgs && nix profile upgrade --all && nix store gc" # Nix
-alias duu="(cd /opt/docker && docker compose pull && docker compose down && docker compose up -d && docker image prune -f)" # Docker (specifikt på min server)
-alias uu="mark 'Apt' && auu && mark 'Nix package manager' && nuu && mark 'Docker' && (cd /opt/docker && docker compose images) && mark"
 
 # Nix package manager alternate commands (the default ones are nonsense)
 alias nxi="nix profile install nixpkgs#"
@@ -68,14 +43,13 @@ alias nxs="nix search nixpkgs"
 alias nxl="nix profile list"
 alias nxug="nix profile upgrade --all"
 alias nxud="nix registry pin nixpkgs"
-alias nxuu="nuu"
+alias nxuu="update -n"
 
 fi
 # ===================================== Arch ======================================
 if [[ $DISTRO == arch ]]; then
 
-# Update commands
-alias uu="mark 'Paru' && paru -Syu && mark"
+
 
 fi
 # =================================================================================
--- sh/functions.zsh
+++ sh/functions.zsh
@@ -1,4 +1,7 @@
 OS=$(uname)
+if [[ $OS == Linux ]]; then
+    DISTRO=$(grep -oP '(?<=^ID=).+' /etc/os-release | tr -d '"')
+fi
 
 # laddar template-funktionen från template-mappen
 source ~/dotfiles/templates/template.zsh
@@ -27,3 +30,98 @@ svt() {
     fi
 }
 
+mark() {
+  local text=${1:+" $1 "}
+  local width=$COLUMNS
+  local prefix=" * "
+  local suffix=" * "
+  local inner=$(( width - ${#prefix} - ${#suffix} ))
+  local pad=$(( (inner - ${#text}) / 2 ))
+  local extra=$(( inner - ${#text} - pad * 2 ))
+  local bar=$(printf '%*s' $inner '' | tr ' ' '=')
+  local left=${bar:0:$pad}
+  local right=${bar:0:$(( pad + extra ))}
+  printf "\n%s%s%s%s%s\n\n" "$prefix" "$left" "$text" "$right" "$suffix"
+}
+
+# ===================================== Linux =====================================
+if [[ $OS == Linux ]]; then
+
+
+
+fi
+# ===================================== macOS =====================================
+if [[ $OS == Darwin ]]; then
+
+# Updates
+update() {
+  local do_brew=0 do_mas=0 do_system=0
+
+  case "$1" in
+    -A) do_brew=1; do_mas=1; do_system=1 ;;
+    -*)
+      [[ "$1" == *b* ]] && do_brew=1
+      [[ "$1" == *m* ]] && do_mas=1
+      [[ "$1" == *s* ]] && do_system=1
+      ;;
+    *)
+      echo "Usage: update -A | -[b][m][s]"
+      return 1
+      ;;
+  esac
+
+  (( do_brew   )) && mark 'Homebrew'        && brew update && brew upgrade && brew cleanup
+  (( do_mas    )) && mark 'Mas'             && mas upgrade
+  (( do_system )) && mark 'Software Update' && sudo softwareupdate -iaR
+}
+
+fi
+# ==================================== Debian =====================================
+if [[ $DISTRO == debian ]]; then
+
+# Updates
+update() {
+  local do_apt=0 do_nix=0 do_docker=0
+
+  case "$1" in
+    -A) do_apt=1; do_nix=1; do_docker=1 ;;
+    -*)
+      [[ "$1" == *a* ]] && do_apt=1
+      [[ "$1" == *n* ]] && do_nix=1
+      [[ "$1" == *d* ]] && do_docker=1
+      ;;
+    *)
+      echo "Usage: update -A | -[a][n][d]"
+      return 1
+      ;;
+  esac
+
+  (( do_apt    )) && mark 'Apt'    && sudo apt update && sudo apt upgrade && sudo apt autoremove
+  (( do_nix    )) && mark 'Nix'    && nix registry pin nixpkgs && nix profile upgrade --all && nix store gc
+  (( do_docker )) && mark 'Docker' && (cd /opt/docker && docker compose pull && docker compose down && docker compose up -d && docker image prune -f)
+}
+
+fi
+# ===================================== Arch ======================================
+if [[ $DISTRO == arch ]]; then
+
+# Updates
+update() {
+  local do_paru=0
+
+  case "$1" in
+    -A) do_paru=1 ;;
+    -*)
+      [[ "$1" == *p* ]] && do_paru=1
+      ;;
+    *)
+      echo "Usage: update -A | -[p]"
+      return 1
+      ;;
+  esac
+
+  (( do_paru )) && mark 'Paru' && paru -Syu
+}
+
+fi
+# =================================================================================
