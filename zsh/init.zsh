a8ef63d13609610bbc2f98b2c76b20adb2890092
carbe814 <carbe814@student.liu.se>
2026-05-27 20:14:07 +0200

    improved zsh completion

--- zsh/init.zsh
+++ zsh/init.zsh
@@ -24,11 +24,6 @@ source ~/dotfiles/zsh/updates.zsh
 source ~/dotfiles/zsh/aliases.zsh
 source ~/dotfiles/zsh/paths.zsh
 
-# Zsh autocomplete
-autoload -Uz compinit && compinit
-zstyle ':completion:*' menu select
-zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z} m:{A-Z}={a-z}'
-
 # Terminal history (replaced by atuin but worth keeping around for autosuggestions)
 HISTFILE=~/.zsh_history
 HISTSIZE=1000
@@ -38,6 +33,15 @@ setopt SHARE_HISTORY
 # Colors
 # https://geoff.greer.fm/lscolors/
 export EZA_COLORS="di=36:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43:uu=33" # eza
-[[ $OS == Linux  ]] && export LS_COLORS="di=36:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43" # linux
+export LS_COLORS="di=36:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43" # linux, zsh completion
 [[ $OS == Darwin ]] && export CLICOLOR=1 && export LSCOLORS=gxfxcxdxbxegedabagacad # macos
 
+# Zsh completion
+autoload -Uz compinit && compinit
+zstyle ':completion:*' menu select
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z} m:{A-Z}={a-z} r:|=* l:|=*'
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+zstyle ':completion:*' group-name ''
+zstyle ':completion:*:descriptions' format '%F{yellow}-- %d --%f'
+zstyle ':completion:*:warnings' format '%F{red}no matches%f'
+zstyle ':completion:*' verbose yes
